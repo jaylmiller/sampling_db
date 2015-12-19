3d3c512c4056d46cea276078ba59de642a1625f6
TrevorAron <taron1@jhu.edu>
2015-12-19 18:35:01 -0500

    messing round with da website

--- samp_map/samp_map.py
+++ samp_map/samp_map.py
@@ -1,20 +1,19 @@
 #imports
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
+from flask.ext.googlemaps import GoogleMaps
+from flask.ext.googlemaps import Map
 import mysql.connector
+import geocoder
 
 #configuration
 DEBUG       = True
 SECRET_KEY  = '13aront'
-USERNAME    = 'admin'
-PASSWORD    = 'default'
 
 #create app
 app = Flask(__name__)
 app.config.from_object(__name__)
-
-if __name__ == '__main__':
-    app.run()
+GoogleMaps(app)
 
 def connect_db():
     return mysql.connector.connect(user='trevoraron', password='dinn3r$12',\
@@ -31,8 +30,29 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
+@app.route("/", methods=['GET', 'POST'])
+def mapview():
+    loc = geocoder.google('USA').latlng
+    mymap = Map(
+        identifier="map",
+        lat=loc[0],
+        lng=loc[1],
+        zoom='4',
+        maptype="TERRAIN",
+        style="height:600px;width:100%;position:absolute;z-index:200;",
+        markers={'http://maps.google.com/mapfiles/ms/icons/green-dot.png':[loc]}
+    )
+    return render_template('temp.html', mymap=mymap)
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
 #@app.route('/stuff', optional--methods=['POST'])
 #to do website:
 #return render_template('template_name.html', optional--template_obj=var)
 #to redirect
 #return redirect(url_for(function))
+
+if __name__ == '__main__':
+    app.run()
