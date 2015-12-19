dfe643a9763529f307b7cd3ed69d5cdd9d663303
TrevorAron <taron1@jhu.edu>
2015-12-18 20:27:45 -0500

    Create database_def.sql
    definitions of the database

---
+++ database_def.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS final;
+CREATE DATABASE final;
+
+USE final;
+
+DROP TABLE IF EXISTS song;
+CREATE TABLE song(
+    audio_md5       VARCHAR(50) NOT NULL,
+    title           VARCHAR(30),
+    artist_id       VARCHAR(30),
+    tempo           FLOAT,
+    s_key           VARCHAR(10),
+    mode            VARCHAR(10),
+    danceability    FLOAT,
+    year            INT,
+    PRIMARY KEY (audio_md5)
+);
+
+DROP TABLE IF EXISTS artist;
+CREATE TABLE artist(
+    artist_id       VARCHAR(30) NOT NULL,
+    name            VARCHAR(30),
+    genre           VARCHAR(20),
+    location        VARCHAR(100),
+    year_start      INT,
+    year_end        INT,
+    PRIMARY KEY (artist_id)
+);
+
+DROP TABLE IF EXISTS sampled;
+CREATE TABLE sampled(
+    song_md5        VARCHAR(50),
+    sampled_md5     VARCHAR(50)
+);
